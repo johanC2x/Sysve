0491d0b68fa575c734f88dc06cb12a430161255f
Johan Cañari <johanc.cca@gmail.com>
2017-10-31 21:47:20 -0500

    MODIFICACION EN MENU DE INICIO

--- application/views/home.php
+++ application/views/home.php
@@ -1,28 +1,33 @@
 <?php $this->load->view("partial/header"); ?>
-<br />
+<br>
 <h3><?php //echo $this->lang->line('common_welcome_message'); ?></h3>
 <div id="home_module_list">
-	<?php $var = 2; ?>
-	<?php foreach($allowed_modules->result() as $module) { ?>
-		<div class="col-md-6">
-			<div class="col-md-4">
-				<a href="<?php echo site_url("$module->module_id");?>">
-					<center>
-						<img src="<?php echo base_url().'images/menubar/'.$module->module_id.'.png';?>" border="0" alt="Menubar Image" />
-					</center>
-				</a>
-			</div>
-			<div class="col-md-8">
-				<a href="<?php echo site_url("$module->module_id");?>">
-					<?php echo $this->lang->line("module_".$module->module_id) ?></a>
-	 				- <?php echo $this->lang->line('module_'.$module->module_id.'_desc');?>
-			</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12" >
+				<?php foreach($allowed_modules->result() as $module) { ?>
+					<div class="col-md-4" >
+						<div class="panel panel-default col-md-10" >
+							<div class="panel-header"> 
+								<center>
+									<img src="<?php echo base_url().'images/menubar/'.$module->module_id.'.png';?>" style="margin: 0 auto;width: 150px;" border="0" alt="Menubar Image" class="img-rounded"> 
+								</center>  
+							</div>
+    						<div class="panel-body">
+    							<a href="<?php echo site_url("$module->module_id");?>">
+    								<center>
+    									<b>
+    										<?php echo strtoupper($this->lang->line("module_".$module->module_id));  ?> 
+    									</b> 
+    								</center>
+								</a>
+	 							<?php //echo $this->lang->line('module_'.$module->module_id.'_desc');?>
+    						</div>
+  						</div>
+					</div>
+				<?php } ?>
+			</div>		
 		</div>
-		<?php if($var === 2){ ?>
-			<?=  "<br/>"; ?>
-			<?php $var = 0;?>
-		<?php } ?>
-		<?php $var++; ?>
-	<?php } ?>
+	</div> 
 </div>
 <?php $this->load->view("partial/footer"); ?>
