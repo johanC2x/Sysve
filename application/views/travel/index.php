0111e833d397328a88880eb785fe6dbc31f50657
Lilia <lilia.chuye@gmail.com>
2018-02-26 23:15:39 -0500

    Vista del modulo travel

--- application/views/travel/index.php
+++ application/views/travel/index.php
@@ -1,30 +1,195 @@
 <?php $this->load->view("partial/header"); ?>
 
-<div id="title_bar">
-	<div id="title" class="float_left">
-		Módulo de Viajes
-	</div>
-	<div id="new_button">
-		Crear Viaje
-	</div>
-</div>
+<div class="wrapper_boxed">
+		<div class="site_wrapper">
+			<header id="header">
+				<div id="trueHeader">
+					<div class="wrapper">
+						<div class="container"> 
 
-<div id="table_holder">
-	<table class="table" >
-		<thead>
-			<tr>
-				<th>a</th>
-				<th>b</th>
-				<th>c</th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<td>a</td>
-				<td>b</td>
-				<td>c</td>
-			</tr>
-		</tbody>
-	</table>
-</div>
+							<!-- Logo -->
+							<div class="logo"><a href="index.html" id="logo"></a></div>
+
+							<!-- Menu -->
+							<div class="menu_main">
+								<div class="navbar yamm navbar-default">
+									<div class="container">
+										<div class="navbar-header">
+											<div class="navbar-toggle .navbar-collapse .pull-right " data-toggle="collapse" data-target="#navbar-collapse-1"  > <span>Menu</span>
+												<button type="button" > <i class="fa fa-bars"></i></button>
+											</div>
+										</div>
+										<div id="navbar-collapse-1" class="navbar-collapse collapse pull-right">
+											<nav>
+												<ul class="nav navbar-nav">
+													<li class="dropdown"> <a href="index.html" class="dropdown-toggle"> Home</a></li>
+													<li class="dropdown"> <a href="http://codelayers.net/foxuhost/layout2/fullwidth/index.html" class="dropdown-toggle"> Layouts</a>
+														<ul class="dropdown-menu" role="menu">
+															<li><a href="http://codelayers.net/foxuhost/layout2/fullwidth/index.html">Layout Light</a> </li>
+															<li><a href="http://codelayers.net/foxuhost/layout3/fullwidth/index.html">Layout Classic</a> </li>
+															<li><a href="http://codelayers.net/foxuhost/layout4/fullwidth/index.html">Layout Light2</a> </li>
+															<li><a href="http://codelayers.net/foxuhost/layout5/fullwidth/index.html">Layout Classic2</a> </li>
+															<li><a href="http://codelayers.net/foxuhost/layout6/fullwidth/index.html">Layout Classic3</a> </li>
+															<li><a href="http://codelayers.net/foxuhost/layout1/fullwidth/index.html">Layout Creative</a> </li>
+															<li><a href="http://codelayers.net/foxuhost/layout7/fullwidth/index.html">Layout One Page</a></li>
+															<li class="dropdown-submenu mul"> <a tabindex="-1" href="#">Header Styles</a>
+																<ul class="dropdown-menu">
+																	<li><a href="http://codelayers.net/foxuhost/layout2/fullwidth/index.html">Header Style1</a> </li>
+																	<li><a href="http://codelayers.net/foxuhost/layout3/fullwidth/index.html">Header Style2</a> </li>
+																	<li><a href="http://codelayers.net/foxuhost/layout4/fullwidth/index.html">Header Style3</a> </li>
+																	<li><a href="http://codelayers.net/foxuhost/layout5/fullwidth/index.html">Header Style4</a> </li>
+																	<li><a href="http://codelayers.net/foxuhost/layout6/fullwidth/index.html">Header Style5</a> </li>
+																	<li><a href="http://codelayers.net/foxuhost/layout1/fullwidth/index.html">Header Style6</a> </li>
+																	<li><a href="http://codelayers.net/foxuhost/layout7/fullwidth/index.html">Header Style7</a> </li>
+																</ul>
+															</li>
+														</ul>
+													</li>
+												</ul>
+											</nav>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
+			<div class="clearfix"></div>
+			<div class="section_holder30">
+				<div class="container">
+					<div class="one_full">
+						<form method="get" id="client-searchform" action="blog.html">
+							<div>
+								<input class="input-text" name="dsearch" id="dsearch3" value="Ingrese nombre del cliente" onFocus="if (this.value == 'Ingrese nombre del cliente') {this.value = '';}" onBlur="if (this.value == '') {this.value = 'Ingrese nombre del cliente';}" type="text" />
+								<button />Buscar</button>
+								<button />Nuevo Cliente</button>
+								<button />Agregar Vuelo</button>
+							</div>
+						</form>
+						<div class="text">Datos...</div>
+					</div>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+			<div class="section_holder19">
+				<div class="container">
+					<div class="one_full">
+						<h3>Registro de Viajes</h3>
+						<div class="table-style">
+							<table class="table-list2">
+									<thead>
+										<tr>
+											<th>O</th>
+											<th>Cliente</th>
+											<th>Dni</th>
+											<th>Pasaporte</th>
+											<th>Expiración</th>
+											<th>F.Nac.</th>
+											<th>Ventana</th>
+											<th>Pasillo</th>
+											<th>Millaje</th>
+											<th>Visa</th>
+											<th>Vacuna</th>
+											<th>Opciones</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td><a>Remover</a>&nbsp;<a>Agregar Comisiones</a>&nbsp;<a>Agregar Vuelo</a></td>
+										</tr>
+										<tr>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td><a>Remover</a>&nbsp;<a>Agregar Comisiones</a>&nbsp;<a>Agregar Vuelo</a></td>
+										</tr>
+										<tr>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td><a>Remover</a>&nbsp;<a>Agregar Comisiones</a>&nbsp;<a>Agregar Vuelo</a></td>
+										</tr>
+										<tr>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td><a>Remover</a>&nbsp;<a>Agregar Comisiones</a>&nbsp;<a>Agregar Vuelo</a></td>
+										</tr>
+									</tbody>
+							</table>
+						</div>
+						<h3>Datos del Pago</h3>
+					</div>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+				<div class="section_holder19">
+					<div class="container">
+						<div class="one_full">
+							<div class="one_fourth">
+								<div>Total Corado</div>
+								<div>Tipo de Tarjeta</div>
+								<div>Numero TC</div>
+								<div>Expiracion</div>
+								<div>CCV</div>
+							</div>
+							<div class="one_fourth">
+								<div><input type="text" name=""></div>
+								<div><input type="text" name=""></div>
+								<div><input type="text" name=""></div>
+								<div><input type="text" name=""></div>
+							</div>
+							<div class="one_fourth">
+								<div>Banco</div>
+								<div>Documento</div>
+								<div>Titular</div>
+								<div>Tipo de Pago</div>
+							</div>
+							<div class="one_fourth last">
+								<div><input type="text" name=""></div>
+								<div><input type="text" name=""></div>
+								<div><input type="text" name=""></div>
+								<div><input type="text" name=""></div>
+							</div>
+						</div>
+					</div>
+				</div>
+		</div>
+	</div>
 <?php $this->load->view("partial/footer"); ?>
