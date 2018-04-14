8b4d95b2bf14f182e501b1fd1fbe6b0807ddb1c6
Johan Cañari Huamani <jacanari@unasolutions.com>
2018-04-13 20:37:05 -0500

    REDIRECCIONANDO A PAGOS

--- application/views/travel/new.php
+++ application/views/travel/new.php
@@ -9,7 +9,7 @@
 						<input type="text" class="form-control" id="code_travel_search" placeholder="Ingresar Código" name="code_travel" />
 					</div>
 					<button type="button" class="btn btn-primary" onclick="travel.getSolicitud()">Buscar Solicitud</button>
-					<a type="button" class="btn btn-primary" href="<?php echo base_url();?>/index.php/travel/payment" >Generar Cobros</a>
+					<a type="button" class="btn btn-primary" href="<?php echo base_url();?>index.php/travel/payment" >Generar Cobros</a>
 					
 				</fieldset>
 			<?php echo form_close(); ?>
