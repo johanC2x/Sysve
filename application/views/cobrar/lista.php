2793971626e137a7a6d95f5aa5bc6a04854e6976
johanC2x <najoh_963@hotmail.com>
2018-04-24 13:20:31 -0500

    ¨cambios¨

--- application/views/cobrar/lista.php
+++ application/views/cobrar/lista.php
@@ -30,7 +30,7 @@
 </div>
 
 <!-- Trigger the modal with a button -->
-<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+<!-- <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button> -->
 
 <!-- Modal -->
 <div id="myModal" class="modal fade" role="dialog">
