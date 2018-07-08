80d3537919d131621efe10dcdd5490298a068d37
Nelson Perez <38821293+nelson909@users.noreply.github.com>
2018-07-08 18:03:17 -0500

    Opcion para Generar Cotizacion

--- application/views/customers/cotizar.php
+++ application/views/customers/cotizar.php
@@ -1,4 +1,8 @@
 <?php $this->load->view("partial/header"); ?>
+
+
+
+
 <script src="<?php echo base_url();?>js/lib/travel.js" type="text/javascript" language="javascript" charset="UTF-8"></script>
 
 <div class="row">
@@ -6,27 +10,28 @@
         <div class="col-md-6">
                 <?php
                     $caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"; //posibles caracteres a usar
-                    $numerodeletras=6; //numero de letras para generar el texto
+                    $numerodeletras=4; //numero de letras para generar el texto
                     $cadena = ""; //variable para almacenar la cadena generada
                             for($i=0;$i<$numerodeletras;$i++) {
                                         $cadena .= substr($caracteres,rand(0,strlen($caracteres)),1); 
                                     }
-                    $fecha = date("dmY");
+                    $fecha = date("dm");
+                    $cotizacion_id = $this->lang->lin.strtoupper("$user_info->first_name")."-".$cadena."-".$fecha."-C";
                 ?>
-        <h4 class="modal-title">Cotizacion Nro: <?php echo "$cadena"."-"."$fecha"; ?> </h4>
+
+        <h4 class="modal-title">Cotizacion Nro: <?php echo $cotizacion_id; ?></h4>
         </div>
     </div>
 </div>
 <hr/>
 <div class="row">
-    <div class="col-md-12">
         <div class="col-md-12">
                 <fieldset>
                     <legend>Datos del Cliente</legend>
                 </fieldset>
     <?php if (isset($datos)): ?>
-            <form id="form_customer_data" role="form">
-                <div class="col-md-4" class="form-group">
+            <form role="form" id="datos_cotizacion">
+                <div class="col-md-2" class="form-group">
                     <label>Nro. de Identificacion</label>
                     <input type="text" id="customer_document" name="customer_document" value="<?php echo $datos['person_id']; ?>" class="form-control" disabled />
                     <input type="hidden" id="customer_id" name="customer_id" />
@@ -36,17 +41,24 @@
                     <input type="text" id="customer_name" name="customer_name" value="<?php echo $datos['firstname']; ?> <?php echo $datos['middlename']; ?> <?php echo $datos['lastname']; ?> <?php echo $datos['mother_lastname']; ?>" class="form-control" disabled/>
                 </div>
                 <div class="col-md-4" class="form-group">
-                    <label>Direccion</label>
-                    <textarea id="customer_address" name="customer_address" class="form-control" placeholder="Dirección" disabled="true"></textarea>
+                    <label>Email</label>
+                    <input id="customer_address" name="customer_address" class="form-control" disabled/>
+                </div>
+                <div class="col-md-2" class="form-group">
+                    <label>Telefono</label>
+                    <input id="customer_address" name="customer_address" class="form-control" placeholder="celular_personal" disabled="true">
+                </div><br>
+                <div class="col-md-12">
+                     <label>Observaciones</label>
+                        <div class="form-group">
+                           <textarea disabled id="descripcion" class="form-control" style="height: 90px;" value="<?php echo $datos['descripcion']; ?>" ></textarea>
+                        </div>
                 </div>
             </form>
     <?php else: echo 'La entrada no existe.'; endif; ?>
         </div>
-
-
-
-    </div>
 </div>
+<hr/>
 <div class="row">
     <div class="col-md-12">
         <?php echo form_open('travel/saveComision',array('id'=>'form_travel_comision_save','class' => 'form-inline')); ?>
@@ -55,16 +67,48 @@
                   <div class="form-group mx-sm-3 mb-2">
                     <select id="cbo_comision_payment" name="cbo_comision_payment" class="form-control">
                         <option value="">Seleccionar Tipo de Servicio</option>
-                        <?php foreach ($property as $key => $value) {?>
-                            <option value="<?= $value["id"]; ?>" data-key="<?= $value["key"]; ?>"><?= $value["name"]; ?></option>
-                        <?php } ?>
+                        <option value="">Vuelo</option>
+                        <option value="">Hotel</option>
+                        <option value="">Auto</option>
+                        <option value="">Seguro</option>
+                        <option value="">Paquete</option>
+                        <option value="">Tours</option>
+                        <option value="">Crucero</option>
+                        <option value="">Trenes</option>
+                        <option value="">Entradas</option>
+                        <option value="">Vuelos de Regreso</option>
+                        <option value="">Otros</option>
                     </select>
                   </div>
                 <div class="form-group">
                     <input type="number" id="amount_travel" name="amount_travel" class="form-control" value="0" style="display: none"/>
                 </div>
                   <button id="btn_save_com" type="button" class="btn btn-primary">Agregar Servicio</button>
-                  <button style="float: right" type="button" class="btn btn-primary btn_save_travel">Guardar Viaje</button>
+
+
+
+
+<input type="button" class="btn btn-primary pull-right" onclick="printDiv('areaImprimir')" value="Generar Cotizacion" />
+
+<script type="text/javascript">
+    function printDiv(nombreDiv) {
+     var contenido= document.getElementById(nombreDiv).innerHTML;
+     var contenidoOriginal= document.body.innerHTML;
+
+     document.body.innerHTML = contenido;
+
+     window.print();
+
+     document.body.innerHTML = contenidoOriginal;
+}
+</script>
+
+
+
+
+
+
+
             </fieldset>
             <div class="alert alert-danger alert-dismissible error_comision"></div>
         <?php echo form_close(); ?>
@@ -78,7 +122,7 @@
                     <tr>
                         <th class="col-md-1"><center>Nro.</center></th>
                         <th class="col-md-4"><center>Servicios</center></th>
-                        <th class="col-md-4"><center>Ticket</center></th>
+                        <th class="col-md-4"><center>Codigo</center></th>
                         <th class="col-md-2"><center>Monto</center></th> 
                         <th colspan="3" class="col-md-1"><center>Acción</center></th>
                     </tr>
@@ -112,6 +156,119 @@
         </div>
     </div>
 </div>
+
+
+<div id="modal_detail_comision" class="modal fade" role="dialog" data-backdrop="static" data-keyboard="false">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Informacion del Servicio - Cotizacion Nro: <?php echo $this->lang->lin.strtoupper("$user_info->first_name"); ?>-<?php echo "$cadena"."-"."$fecha"; ?>-C</h4>
+      </div>
+            <div class="modal-body">
+            <?php echo form_open('travel/updateDetailComision',array('id'=>'form_travel_comision_update')); ?>
+
+<br/>
+
+                        <div class="col-md-4" class="form-group">
+                            <label for="code_travel">Servicio:</label>
+                            <input type="text" name="code_travel" id="code_travel" class="form-control" />
+                        </div>
+                         <div class="col-md-4" class="form-group">
+                            <label for="name_travel">Codigo:</label>
+                            <input type="text" name="name_travel" id="name_travel" class="form-control" />
+                        </div>
+                         <div class="col-md-4" class="form-group">
+                            <label for="total_servicios">Monto:</label>
+                            <input type="text" id="total_servicios" name="total_servicios" class="form-control" />
+                        </div>
+
+
+            <!-- =========== FORM ADDRESS ============ -->
+            <div class="col-md-12">
+              <fieldset>
+                <legend>&nbsp;</legend>
+                <div class="form-group">
+                  <textarea id="descripcion" class="form-control" style="height: 300px;"></textarea>
+                </div>
+              </fieldset>
+            </div>
+            <!-- ===================================== -->
+
+
+            <div class="form-group">
+                    <input type="file" name="">
+            </div>
+                <?php echo form_close(); ?>
+      <div class="modal-footer">
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary pull-left" >Siguiente</button> 
+                <button type="button" onclick="travel.cancelRegisterCustomer();" class="btn btn-default" data-dismiss="modal">Cerrar</button>      
+            </div>
+
+      </div>
+
+            </div>
+            </div>
+            
+        </div>
+
+
+<div class="col-md-10" id="areaImprimir">
+
+<div class="col-md-6">
+    <img src="http://mundigea.com.pe/tpl/img/usuarios/666/logo1_img_250x100.png">
+</div>
+<br>
+            <h4 class="modal-title">Cotizacion Nro: <?php echo $cotizacion_id; ?></h4>
+<br>
+                <div class="col-md-2" class="form-group">
+                    <label>Nro. de Identificacion</label>
+                    <input type="text" id="customer_document" name="customer_document" value="<?php echo $datos['person_id']; ?>" class="form-control" disabled />
+                    <input type="hidden" id="customer_id" name="customer_id" />
+                </div>
+                <div class="col-md-4" class="form-group">
+                    <label>Nombres y Apellidos</label>
+                    <input type="text" id="customer_name" name="customer_name" value="<?php echo $datos['firstname']; ?> <?php echo $datos['middlename']; ?> <?php echo $datos['lastname']; ?> <?php echo $datos['mother_lastname']; ?>" class="form-control" disabled/>
+                </div>
+                <div class="col-md-4" class="form-group">
+                    <label>Email</label>
+                    <input id="customer_address" name="customer_address" class="form-control" disabled/>
+                </div>
+                <div class="col-md-2" class="form-group">
+                    <label>Telefono</label>
+                    <input id="customer_address" name="customer_address" class="form-control" placeholder="celular_personal" disabled="true">
+                </div>
+    <div class="col-md-12">
+        <br/>
+        <form>
+            <table id="table_customer_travel" class="table table-hover table-bordered" >
+                <thead>
+                    <tr>
+                        <th class="col-md-1"><center>Nro.</center></th>
+                        <th class="col-md-4"><center>Servicios</center></th>
+                        <th class="col-md-4"><center>Codigo</center></th>
+                        <th class="col-md-2"><center>Monto</center></th> 
+                        <th colspan="3" class="col-md-1"><center>Acción</center></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td colspan="5">
+                            <center>
+                                No se registraron datos.
+                            </center>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+
+        </form>
+    </div>
+
+</div>
+
+
 
 
 <?php $this->load->view("travel/modal"); ?>
@@ -197,6 +354,28 @@
             });
         }
 
+        function imprimir_cotizacion(){
+            VentanaCentrada('./pdf/documentos/ver_factura.php?id_factura='+id_cotizacion,'Factura','','1024','768','true');
+        }
+
+
     });
 
 </script>
+<?php $this->load->view("partial/footer"); ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
